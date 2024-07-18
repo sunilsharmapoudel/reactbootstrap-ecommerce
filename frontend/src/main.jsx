5df58287b7f2915a0060ef20dede8aa1aebbf6e7
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-07-18 07:42:37 +0545

    map managed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import HomePage from "./pages/HomePage.jsx";
 import ProductPage from "./pages/HomePage.jsx";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <App />,
-  children: [
-    {
-      path: "",
-      element: <HomePage />
-    },
-    {
-      path: "product",
-      element: <ProductPage />
-    },
-  ]
-},
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "",
+//         element: <HomePage />
+//       },
+//       {
+//         path: "product",
+//         element: <ProductPage />
+//       },
+//     ],
+//   },
+// ])
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App />}>
+    <Route path="" element={<HomePage />} />
+    <Route path="product" element={<ProductPage />} />
+  </Route>
+))
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
